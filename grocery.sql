bd84dd37cd2d80c30a6914ffc7c2b4052360511f
Lawton Stephens <stephens.lawton@gmail.com>
2023-08-20 21:10:34 -0400

    grocery list

---
+++ grocery.sql
@@ -0,0 +1,32 @@
+
+INSERT INTO groceries (item) 
+VALUES 
+('Bananas'),
+('Snack apples'),
+('Honey crisp apples'),
+('Celery'),
+('Carrots (might have)'),
+('Red pepper'),
+('Fresh dill'),
+('Red onion'),
+('One lemon'),
+('Cilantro'),
+('Green onion'),
+('Garlic'),
+('Red potatoes'),
+('Two avocados '),
+('Curly kale'),
+('Cashew butter'),
+('Macaroni noodles'),
+('chickpeas'),
+('Pickles'),
+('Pickled jalapeños'),
+('Powdered sugar'),
+('Cream cheese'),
+('Frozen peas (might have)'),
+('Gluten free brown rice fusilli'),
+('Coconut milk'),
+('Gf AP flour'),
+('Cane sugar'),
+('Choc chips');
+
