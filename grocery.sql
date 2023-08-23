7c17f87732e2f54fac622d5afc3d08abcd32a780
Lawton Stephens <stephens.lawton@gmail.com>
2023-08-22 23:26:40 -0400

    sql practice

--- grocery.sql
+++ grocery.sql
@@ -1,32 +1,44 @@
 
-INSERT INTO groceries (item) 
-VALUES 
-('Bananas'),
-('Snack apples'),
-('Honey crisp apples'),
-('Celery'),
-('Carrots (might have)'),
-('Red pepper'),
-('Fresh dill'),
-('Red onion'),
-('One lemon'),
-('Cilantro'),
-('Green onion'),
-('Garlic'),
-('Red potatoes'),
-('Two avocados '),
-('Curly kale'),
-('Cashew butter'),
-('Macaroni noodles'),
-('chickpeas'),
-('Pickles'),
-('Pickled jalapeños'),
-('Powdered sugar'),
-('Cream cheese'),
-('Frozen peas (might have)'),
-('Gluten free brown rice fusilli'),
-('Coconut milk'),
-('Gf AP flour'),
-('Cane sugar'),
-('Choc chips');
+-- INSERT INTO groceries (item) 
+-- VALUES 
+-- ('Bananas'),
+-- ('Snack apples'),
+-- ('Honey crisp apples'),
+-- ('Celery'),
+-- ('Carrots (might have)'),
+-- ('Red pepper'),
+-- ('Fresh dill'),
+-- ('Red onion'),
+-- ('One lemon'),
+-- ('Cilantro'),
+-- ('Green onion'),
+-- ('Garlic'),
+-- ('Red potatoes'),
+-- ('Two avocados '),
+-- ('Curly kale'),
+-- ('Cashew butter'),
+-- ('Macaroni noodles'),
+-- ('chickpeas'),
+-- ('Pickles'),
+-- ('Pickled jalapeños'),
+-- ('Powdered sugar'),
+-- ('Cream cheese'),
+-- ('Frozen peas (might have)'),
+-- ('Gluten free brown rice fusilli'),
+-- ('Coconut milk'),
+-- ('Gf AP flour'),
+-- ('Cane sugar'),
+-- ('Choc chips');
+
+
+-- CREATE TABLE books (
+--     book_id serial PRIMARY KEY,
+--     title text,
+--     author text, 
+--     genre text, 
+--     price numeric, 
+--     in_stock integer
+-- ) ;
+
+
 
